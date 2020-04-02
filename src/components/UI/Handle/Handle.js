3e09485930cd52423bf997d385a31843debbad66
pinglu85 <luping8576@hotmail.com>
2020-04-02 23:28:46 +0200

    put line tag before circle

--- src/components/UI/Handle/Handle.js
+++ src/components/UI/Handle/Handle.js
@@ -4,14 +4,6 @@ import PropTypes from 'prop-types';
 const Handle = ({ handlePos }) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 500">
-      <circle
-        cx={handlePos.x}
-        cy={handlePos.y}
-        r="5"
-        fill="#fff"
-        stroke="#6c7489"
-        strokeMiterlimit="10"
-      ></circle>
       <line
         x1={handlePos.x1}
         y1={handlePos.y1}
@@ -21,6 +13,14 @@ const Handle = ({ handlePos }) => {
         strokeWidth="2"
         strokeDasharray="1"
       />
+      <circle
+        cx={handlePos.x}
+        cy={handlePos.y}
+        r="7"
+        fill="#fff"
+        stroke="#6c7489"
+        strokeMiterlimit="10"
+      ></circle>
     </svg>
   );
 };
