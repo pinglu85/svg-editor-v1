2f981c412c85de310f32c802fcdbbdf1f90005e2
pinglu85 <luping8576@hotmail.com>
2020-04-02 23:30:37 +0200

    change point data structure

--- src/components/SVG/SVG/SVG.js
+++ src/components/SVG/SVG/SVG.js
@@ -7,21 +7,21 @@ import Handle from '../../UI/Handle/Handle';
 const SVG = ({ d, pointsPos, clicked }) => {
   const handle = pointsPos.map((pos, index) => {
     if (pos.c) {
-      if (pos.c.x1 !== 0) {
+      if (pos.c[0].x !== 0) {
         return (
           <Fragment key={index}>
             <Handle
               handlePos={{
-                x: pointsPos[index].c.x1,
-                y: pointsPos[index].c.y1,
+                x: pointsPos[index].c[0].x,
+                y: pointsPos[index].c[0].y,
                 x1: pointsPos[index - 1].x,
                 y1: pointsPos[index - 1].y
               }}
             />
             <Handle
               handlePos={{
-                x: pointsPos[index].c.x2,
-                y: pointsPos[index].c.y2,
+                x: pointsPos[index].c[1].x,
+                y: pointsPos[index].c[1].y,
                 x1: pointsPos[index].x,
                 y1: pointsPos[index].y
               }}
